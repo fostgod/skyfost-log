b57ff75a73764f684821c22f23ba40b1b23b7a27
fostgod <82456030+fostgod@users.noreply.github.com>
2021-07-25 16:58:50 +0700

    Update log.js

--- src/log.js
+++ src/log.js
@@ -1,4 +1,6 @@
-module.exports = (log) => {
+var colors = require('colors');
+
+const error = (e) => {
 	var currentdate = new Date(); 
 	var datetime = currentdate.getDate() + "/"
 					+ (currentdate.getMonth()+1)  + "/" 
@@ -6,6 +8,23 @@ module.exports = (log) => {
 					+ currentdate.getHours() + ":"  
 					+ currentdate.getMinutes() + ":" 
 					+ currentdate.getSeconds();
-    let datelog = datetime + log;
-    return datelog
+    let dateerr = datetime +" | "+ e;
+    return colors.red(dateerr)
 }
+
+const log = (log) => {
+	var currentdate = new Date(); 
+	var datetime = currentdate.getDate() + "/"
+					+ (currentdate.getMonth()+1)  + "/" 
+					+ currentdate.getFullYear() + " "  
+					+ currentdate.getHours() + ":"  
+					+ currentdate.getMinutes() + ":" 
+					+ currentdate.getSeconds();
+    let datelog = datetime +" | "+ log;
+    return colors.green(datelog)
+}
+
+module.exports = {
+	log,
+	error
+};
